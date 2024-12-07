4d214b3209e5628b6a44d5a71163b4c40dfb3956
Kevin Daniel Martínez <kevin.rodriguez.2001m@gmail.com>
2024-12-06 18:40:32 -0600

    Optimización de código
    Se eliminaron espacios en blanco y se ordeno elementos en diferentes carpetas

---
+++ src/data/proyectData.js
@@ -0,0 +1,25 @@
+const projects = [
+  {
+    image: "https://pub-c0f5f950f13841e8b14919b84237d411.r2.dev/p1.webp",
+    title: "Techo de Tejas",
+    description:
+      "En el corazón de una exclusiva comunidad residencial, transformamos una villa de estilo mediterráneo con un techo de teja de terracota premium. Este proyecto incluyó la instalación de tejas personalizadas con un diseño ondulado, resistentes a todo clima.",
+    link: "#proyectos-1",
+  },
+  {
+    image: "https://pub-c0f5f950f13841e8b14919b84237d411.r2.dev/p2.webp",
+    title: "Techo Moderno",
+    description:
+      "Ubicado en una zona residencial, este moderno techo combina resistencia y diseño. Fue construido con paneles térmicos de última tecnología para ofrecer aislamiento superior.",
+    link: "#proyectos-2",
+  },
+  {
+    image: "https://pub-c0f5f950f13841e8b14919b84237d411.r2.dev/p3.webp",
+    title: "Techo Personalizado",
+    description:
+      "En este proyecto exclusivo, instalamos un techo de diseño personalizado con un acabado único, combinando funcionalidad y estilo para satisfacer los gustos más exigentes.",
+    link: "#proyectos-3",
+  },
+];
+
+export default projects;
